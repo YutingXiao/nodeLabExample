69d3e29fd8ebea1c2cb6b138e83dc061b7c7f6c7
xiaox232 <xiaox232@morris.umn.edu>
2015-01-27 19:22:16 -0600

    We wrote a function called numberChanger to change the string entries for grade into a number. We then changed the gpaFinder function to includ the numberChanger function, and we realized that the numbers were being treated as strings, so we added Number() around the number variables.

--- serverJavascript.js
+++ serverJavascript.js
@@ -10,32 +10,32 @@ exports.stringValue = function(str){
 };
 
 exports.gpaFinder = function(number,str,number2,str2,number3,str3){
-  new1 = numbergrade(str);
-  new2 = numbergrade(str2);
-  new3 = numbergrade(str3);
+    var new1 = numberChanger(str);
+    var new2 = numberChanger(str2);
+    var new3 = numberChanger(str3);
 
-  gpa = ((new1*number + new2*number2 + new3*number3)/(number+number2+number3));
+  top = new1*Number(number) + new2*Number(number2) + new3*Number(number3);
+  bottom = Number(number) + Number(number2) + Number(number3);
 
-  return gpa;
+  return top/ bottom;
 };
 
-var numbergrade = function(input){
-    switch (input){
-        case "a"||"A":
-            input = 4.0;
-            break;
-        case "b"||"B":
-            input = 3.0;
-            break;
-        case "c"||"C":
-            input = 2.0;
-            break;
-        case "d"||"D":
-            input = 1.0;
-            break;
-        case "f"||"F":
-            input = 0.0;
-            break;
+numberChanger = function(string){
+    if(string=='a' || string=='A'){
+        string = 4.0;
     }
-    return input;
-}
+    else if(string=='b' || string=='B'){
+        string = 3.0;
+    }
+    else if(string=='c' || string=='C'){
+        string = 2.0;
+    }
+    else if(string=='d' || string=='D'){
+        string = 1.0;
+    }
+    else{
+        string = 0;
+    }
+
+    return string;
+};
